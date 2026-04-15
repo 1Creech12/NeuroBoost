a2ffdca3a644d1a74297c5b1f2fccf12137f5dd4
Creech <167149547+1Creech12@users.noreply.github.com>
2026-04-16 00:06:57 +0900

    Update Timer.cpp

--- Timer/Timer.cpp
+++ Timer/Timer.cpp
@@ -5,16 +5,22 @@ Timer::Timer() : seconds(0), running(true) {
     t = thread([this]() {
         while (running) {
             this_thread::sleep_for(chrono::seconds(1));
-            seconds++;
+            if (running) {
+                seconds++;
+            }
         }
     });
 }
 
 Timer::~Timer() {
     running = false;
-    t.join();
+    if (t.joinable()) {
+        t.join();
+    }
 }
 
 void Timer::reset() { seconds = 0; }
 
 int Timer::get() const { return seconds; }
+
+void Timer::stop() { running = false; }
