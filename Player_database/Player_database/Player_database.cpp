50c664fb68bcf6b7082e54ca30a3675b71702c4b
VolodyPRO2008 <14kalibr978@gmail.com>
2026-04-15 10:15:46 +0900

    Только папка Player_database

---
+++ Player_database/Player_database/Player_database.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+#include <windows.h>
+#include "include.h"
+#include "Data_Player.h"
+
+
+
+
+
+int main()
+{
+    setlocale(0, "Ru");
+    std::cout << "Hello World!\n";
+    sqlite3* bd_player;
+    sqlite3_open("player_bd.db", &bd_player);
+    
+    Data_Player person1;
+
+    //person1.setLogin("UFDLF");
+
+    person1.deleteById(bd_player, 2);
+    person1.saveToBD(bd_player);
+    return 0;
+    
+
+
+}
+
+
---
+++ Player_database/Player_database/include.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "sqlite3.h"
+using namespace std;
+
+
+
+
